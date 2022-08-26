c2792726405871e3fdd72f7e00fbc2525f3fdf92
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-08-27 00:25:38 +0900

    Add `folke/todo-comments.nvim` plugin

--- user_plugins.lua
+++ user_plugins.lua
@@ -33,5 +33,11 @@ return {
     tag = "v1.*",
   },
   { "~/PrivateProjects/playground/neovim-plugins/theme-sync"},
-
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  },
 }
