a93b07fb6f0444a5911c1663dbb3e72baccb1854
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2023-02-07 10:24:45 +0100

    fix: use stable branch

--- init.lua
+++ init.lua
@@ -3,7 +3,9 @@ local config = {
   -- Configure AstroNvim updates
   updater = {
     remote = "origin", -- remote to use
-    channel = "nightly", -- "stable" or "nightly"
+    channel = "stable", -- "stable" or "nightly"
+    -- Nightly is currently broken. 2023-01-16
+    -- channel = "nightly", -- "stable" or "nightly"
     version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
     branch = "main", -- branch name (NIGHTLY ONLY)
     commit = nil, -- commit hash (NIGHTLY ONLY)
